734538d9b5189b80f1ea5afdf36f18136c02ed3a
Fahmin Guliyev <baku.fehmin@gmail.com>
2021-04-19 00:51:41 +0400

    added minimal standard tools

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function init() {
   });
 
   const source = new ol.source.Vector();
+
   const vector = new ol.layer.Vector({
     source: source,
     style: new ol.style.Style({
@@ -15,7 +16,7 @@ function init() {
       }),
       stroke: new ol.style.Stroke({
         color: '#ffcc33',
-        width: 2,
+        width: 3,
       }),
       image: new ol.style.Circle({
         radius: 7,
@@ -25,19 +26,6 @@ function init() {
       }),
     }),
   });
-
-  const map = new ol.Map({
-    view: new ol.View({
-      center: [5316116.203085163, 4921349.362210683],
-      zoom: 7,
-    }),
-    layers: [raster, vector],
-    target: 'js-map',
-  });
-
-  // map.on('click', function (e) {
-  //   console.log(e.coordinate);
-  // });
 
   // Layers
   const OSMStandard = new ol.layer.Tile({
@@ -68,6 +56,25 @@ function init() {
   const baseLayerGroup = new ol.layer.Group({
     layers: [OSMStandard, OSMHumanitarian, stamenTerrain],
   });
+
+  const map = new ol.Map({
+    view: new ol.View({
+      center: [5293437.691331564, 4928767.585347839],
+      zoom: 8,
+    }),
+    layers: [
+      // new ol.layer.Tile({
+      //   source: new ol.source.OSM(),
+      // }),
+      raster,
+      vector,
+    ],
+    target: 'js-map',
+  });
+
+  // map.on('click', function (e) {
+  //   console.log(e.coordinate);
+  // });
 
   map.addLayer(baseLayerGroup);
 
@@ -102,12 +109,54 @@ function init() {
     map.addInteraction(snap);
   }
 
-  // Handle change event
+  function activatePointerMode() {
+    map.removeInteraction(draw);
+    map.removeInteraction(snap);
+    map.removeInteraction(modify);
+    document.getElementById('osm-standard').checked = true;
+  }
+
+  // Select draw tool
   typeSelect.onchange = function () {
     map.removeInteraction(draw);
     map.removeInteraction(snap);
     addInteractions();
+    document.getElementById('draw').checked = true;
   };
 
-  addInteractions();
+  // Handle radio button selections between pointer and draw mode
+  document.getElementById('draw').addEventListener('change', addInteractions);
+  document
+    .getElementById('pointer')
+    .addEventListener('change', activatePointerMode);
+
+  // Handle zoom in
+  document.getElementById('zoomInBtn').addEventListener('click', () => {
+    map.getView().animate({
+      zoom: map.getView().getZoom() + 1,
+      duration: 250,
+    });
+  });
+
+  // Handle zoom out
+  document.getElementById('zoomOutBtn').addEventListener('click', () => {
+    map.getView().animate({
+      zoom: map.getView().getZoom() - 1,
+      duration: 250,
+    });
+  });
+
+  // W3 Fullscreen API with cross-browser availability
+  document.getElementById('fullScreenBtn').addEventListener('click', () => {
+    var mapElement = document.getElementById('js-map');
+    if (mapElement.requestFullscreen) {
+      mapElement.requestFullscreen();
+    } else if (mapElement.webkitRequestFullscreen) {
+      mapElement.webkitRequestFullscreen();
+    } else if (mapElement.mozRequestFullScreen) {
+      mapElement.mozRequestFullScreen();
+    } else if (mapElement.msRequestFullscreen) {
+      mapElement.msRequestFullscreen();
+    }
+  });
 }
